332d0c51f3cd9d1e72f931b821b3867d472f0dda
SONGmINjIJI <20191777@o365.hanbat.ac.kr>
2022-12-05 14:55:29 +0900

    화폐 개수 세기

---
+++ src/main/java/Count_Money.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Count_Money {
+    public static void main(String[] args) {
+        int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+        int[] count = new int[8];
+        Scanner scan = new Scanner(System.in);
+        System.out.print("금액을 넣으세요:");
+        int input = scan.nextInt();
+        for (int i = 0; i < money.length; i++) {
+           count[i] = input/money[i];
+           input %= money[i];
+           System.out.println(money[i] + "원" + count[i] + "개");
+        }
+    }
+}
