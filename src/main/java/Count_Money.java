b9db4b3b7e0b26fdfed3f201849b6c9ba40d867d
SONGmINjIJI <20191777@o365.hanbat.ac.kr>
2022-12-05 15:13:03 +0900

    Refactor: 불필요한 변수 제거

--- src/main/java/Count_Money.java
+++ src/main/java/Count_Money.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Count_Money {
     public static void main(String[] args) {
         int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
-        int[] count = new int[8];
+        int count = 0;
         Scanner scan = new Scanner(System.in);
         System.out.print("금액을 넣으세요:");
         int input = scan.nextInt();
         for (int i = 0; i < money.length; i++) {
-           count[i] = input/money[i];
+           count = input/money[i];
            input %= money[i];
-           System.out.println(money[i] + "원" + count[i] + "개");
+           System.out.println(money[i] + "원" + count + "개");
         }
     }
 }
